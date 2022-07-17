5b2edf1f06832c6219efe5521a7cf914251a6486
Felipe Santana <santanafelipe.dev@gmail.com>
2022-07-16 22:39:52 -0300

    Add select ao formulário de contato

--- src/common/ContactForm/ContactForm.jsx
+++ src/common/ContactForm/ContactForm.jsx
@@ -4,36 +4,69 @@ import './ContactForm.css'
 import Input from '../Input'
 import Textarea from '../Textarea'
 import Button from '../Button/Button'
+import CustomSelect from '../CustomSelect/CustomSelect'
 
-const ContactForm = props => (
-    <div className="ContactForm">
-        <h1 className="headingXl text-center mb-3 c-primary">Entre em contato</h1>
-        <form>
-            <div className="row mb-3">
-                <div className="col-md-6">
-                    <Input label="Nome" />
+const ContactForm = props => {
+    const options = [
+        {
+            value: 'TRATAMENTO_FITOSSANITARIO',
+            title: 'Tratamento Fitossanitário',
+        },
+        {
+            value: 'CONTROLE_DE_PRAGAS_URBANAS',
+            title: 'Controle de Pragas Urbanas'
+        },
+        {
+            value: 'LIMPEZA_E_DESINFECCAO_DE_CAIXA_DAGUA',
+            title: 'Limpeza e Desinfecção de Caixa D\'água'
+        },
+        {
+            value: 'SANITIZACAO_DE_AMBIENTES',
+            title: 'Sanitização de Ambientes'
+        },
+        {
+            value: 'SANEAMENTO_VEGETAL',
+            title: 'Saneamento Vegetal'
+        }
+    ]
+
+    return (
+        <div className="ContactForm">
+            <h1 className="headingXl text-center mb-3 c-primary">Entre em contato</h1>
+            <form>
+                <div className="row mb-3">
+                    <div className="col-md-6">
+                        <Input label="Nome" />
+                    </div>
+                    <div className="col-md-6">
+                        <Input label="Nº celular" />
+                    </div>
                 </div>
-                <div className="col-md-6">
-                    <Input label="Nº celular" />
+                <div className="row mb-4">
+                    <div className="col-md-12">
+                        <Input type="email" label="E-mail" />
+                    </div>
                 </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col-md-12">
-                    <Input type="email" label="E-mail" />
+                <div className="row mb-4">
+                    <div className="col-md-9">
+                        <CustomSelect
+                            label="Solicite orçamento"
+                            options={options} />
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <Textarea label="Digite sua mensagem aqui" />
+                <div className="row">
+                    <div className="col-md-12">
+                        <Textarea label="Digite sua mensagem aqui" />
+                    </div>
                 </div>
-            </div>
-            <div className="d-flex">
-                <Button variant="material" type="submit" size="lg">
-                    Enviar
-                </Button>
-            </div>
-        </form>
-    </div>
-)
+                <div className="d-flex">
+                    <Button variant="material" type="submit" size="lg">
+                        Enviar
+                    </Button>
+                </div>
+            </form>
+        </div>
+    )
+}
 
 export default ContactForm
---
+++ src/common/CustomSelect/CustomSelect.jsx
@@ -0,0 +1,63 @@
+import React, { useCallback, useState } from 'react'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+
+import './CustomSelect.css'
+import { useEffect } from 'react'
+
+const CustomSelect = props => {
+    const [ currentIndex, setCurrentIndex ] = useState(0)
+    const [ value, setValue ] = useState('')
+    const [ open, setOpen ] = useState(false)
+
+    useEffect(() => {
+        setValue(props.options[currentIndex].value || '')
+    }, [setValue, currentIndex, props.options])
+
+    const renderItem = useCallback((o, i) => {
+        if (currentIndex === i)
+            return null
+        
+        return (
+            <li
+                key={`selectItem${i}`}
+                className="option"
+                onClick={ () => setCurrentIndex(i) }>{o.title}</li>
+        )
+    }, [currentIndex, setCurrentIndex])
+
+    const renderOption = useCallback((o, i) => (
+        <option key={`o${i}`} value={o.value}>{ o.title }</option>
+    ), [currentIndex])
+
+    const options = props.options || []
+
+    return (
+        <div className="CustomSelect">
+            { props.label && <label htmlFor={props.name}>{props.label}</label>}
+            <div className="select" onClick={() => setOpen(val => !val)}>
+                <span className="current">{ options[currentIndex].title || '' }</span>
+                <FontAwesomeIcon className="caret" icon={ open ? faCaretDown : faCaretRight } size="1x" color="var(--primary-color)" />
+                <ul className={ `options ${open ? 'open' : ''}` }>
+                    { options.map(renderItem) }
+                </ul>
+            </div>
+            <select
+                value={value}
+                onChange={ e => setValue(e.target.value) }
+                className="hidden" {...props.input}>
+                { options.map(renderOption) }
+            </select>
+        </div>
+    )
+}
+
+CustomSelect.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.shape({
+        value: PropTypes.string,
+        title: PropTypes.string
+    }))
+}
+
+export default CustomSelect
