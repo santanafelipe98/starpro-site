28e383bf03eb3d95b23f88398e0fb0837c07c1c6
Felipe Santana <santanafelipe.dev@gmail.com>
2022-07-16 22:52:48 -0300

    fix: select não fecha ao clicar fora do elemento

--- src/common/CustomSelect/CustomSelect.jsx
+++ src/common/CustomSelect/CustomSelect.jsx
@@ -5,11 +5,28 @@ import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 
 import './CustomSelect.css'
 import { useEffect } from 'react'
+import { useRef } from 'react'
 
 const CustomSelect = props => {
     const [ currentIndex, setCurrentIndex ] = useState(0)
     const [ value, setValue ] = useState('')
     const [ open, setOpen ] = useState(false)
+    const ref = useRef(null)
+
+    useEffect(() => {
+        const handler = e => {
+            const { target } = e
+            
+            if (ref && ref.current) {
+                if (!ref.current.contains(target))
+                    setOpen(false)
+            }
+        }
+    
+        document.addEventListener('click', handler)
+
+        return () => document.removeEventListener('click', handler)
+    }, [ref, setOpen])
 
     useEffect(() => {
         setValue(props.options[currentIndex].value || '')
@@ -34,7 +51,7 @@ const CustomSelect = props => {
     const options = props.options || []
 
     return (
-        <div className="CustomSelect">
+        <div ref={ref} className="CustomSelect">
             { props.label && <label htmlFor={props.name}>{props.label}</label>}
             <div className="select" onClick={() => setOpen(val => !val)}>
                 <span className="current">{ options[currentIndex].title || '' }</span>
